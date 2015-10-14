35ace475fdaa792a27c58b590ebb655d3f327736
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-14 21:03:24 +0200

    created singleton.factory

---
+++ src/main/java/es/upm/miw/pd/singleton/factory/ReferencesFactory.java
@@ -0,0 +1,37 @@
+package es.upm.miw.pd.singleton.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReferencesFactory {
+    private Map<String, Integer> references;
+
+    private int reference;
+    
+    private static ReferencesFactory referencesFactory=null;
+
+    public ReferencesFactory() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+
+    public static ReferencesFactory getFactory(){
+        if (referencesFactory== null){
+            referencesFactory = new ReferencesFactory();
+        }
+        return referencesFactory;
+    }
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+}
