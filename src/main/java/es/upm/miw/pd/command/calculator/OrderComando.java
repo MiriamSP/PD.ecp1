e501c5071f0b2a28f3404f1fa251775817b87fb1
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-18 19:39:44 +0200

    commit OrderComando

--- src/main/java/es/upm/miw/pd/command/calculator/OrderComando.java
+++ src/main/java/es/upm/miw/pd/command/calculator/OrderComando.java
@@ -8,20 +8,14 @@ public abstract class OrderComando implements Comando {
         this.calculator = calculator;
     }
 
-   
-
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public String name() {
-        // TODO Auto-generated method stub
         return null;
     }
-    
-    
 
 }
