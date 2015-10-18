3c90b0b1cbe98479783dcc05c33a3c3789e7f546
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-18 14:27:31 +0200

    commit Clases Compuestas

--- src/main/java/es/upm/miw/pd/text/Parrafo.java
+++ src/main/java/es/upm/miw/pd/text/Parrafo.java
@@ -1,5 +1,29 @@
 package es.upm.miw.pd.text;
 
-public class Parrafo {
+public class Parrafo extends CompositeDocument {
+
+    @Override
+    public String dibujar(Boolean formatMayusculas) {
+        // TODO Auto-generated method stub
+        return super.dibujar(formatMayusculas);
+    }
+
+    @Override
+    protected String printEndLineFeed() {
+        // TODO Auto-generated method stub
+        return "\n";
+
+    }
+
+    @Override
+    public void add(Componente componente) {
+        // TODO Auto-generated method stub
+        componentes.add(componente);
+    }
+
+    @Override
+    public boolean isComposite() {
+        return true;
+    }
 
 }
--- src/main/java/es/upm/miw/pd/text/Texto.java
+++ src/main/java/es/upm/miw/pd/text/Texto.java
@@ -1,5 +1,32 @@
 package es.upm.miw.pd.text;
 
-public class Texto {
+public class Texto extends CompositeDocument {
+
+    @Override
+    public String dibujar(Boolean formatMayusculas) {
+        // TODO Auto-generated method stub
+        return super.dibujar(formatMayusculas);
+    }
+
+    @Override
+    protected String printEndLineFeed() {
+        // TODO Auto-generated method stub
+        return "---o---";
+    }
+
+    @Override
+    public void add(Componente componente) {
+        // TODO Auto-generated method stub
+        componentes.add(componente);
+    }
+
+    @Override
+    public boolean isComposite() {
+        return true;
+    }
+
+   
+
+    
 
 }
